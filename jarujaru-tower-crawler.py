54394e52af561c9701345b84a15be43ad57da005
kanta-nakamura <knkmr0705@gmail.com>
2021-12-24 12:40:14 +0900

    [update] プレイリストの全ての動画タイトルを表示

--- jarujaru-tower-crawler.py
+++ jarujaru-tower-crawler.py
@@ -1,4 +1,5 @@
 import os
+import time
 from apiclient.discovery import build
 
 YOUTUBE_API_KEY = os.environ['YOUTUBE_API_KEY']
@@ -6,11 +7,22 @@ JARUJARU_TOWER_PLAYLIST_ID = 'PLRdiaanKAFQliJh8AMvlV6t7NBrmNXCo-'
 
 youtube = build('youtube', 'v3', developerKey = YOUTUBE_API_KEY)
 
-playlist_items_response = youtube.playlistItems().list(
-    part='snippet',
-    playlistId=JARUJARU_TOWER_PLAYLIST_ID,
-    maxResults=50
-).execute()
+next_page_token = None
 
-for i in range(50):
-    print(playlist_items_response['items'][i]['snippet']['title'])
+while True:
+    playlist_items_response = youtube.playlistItems().list(
+        part='snippet',
+        playlistId=JARUJARU_TOWER_PLAYLIST_ID,
+        maxResults=50,
+        pageToken=next_page_token
+    ).execute()
+
+    for i in range(len(playlist_items_response['items'])):
+        print(playlist_items_response['items'][i]['snippet']['title'])
+
+    if 'nextPageToken' in playlist_items_response.keys():
+        next_page_token = playlist_items_response['nextPageToken']
+    else:
+        break
+
+    time.sleep(3)
